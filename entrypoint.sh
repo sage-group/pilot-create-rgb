5b9b42f674fa62280103fb35915d1d2b051f18a3
Marian Neagul <marian@info.uvt.ro>
2020-02-14 12:53:39 +0200

    properly escape values

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 #export WPS_INPUT_GreenBand="http://thor.sage.ieat.ro/~marian/pilot/T34TDT_20190701T093041_B03_10m.jp2"
 #export WPS_INPUT_BlueBand="http://thor.sage.ieat.ro/~marian/pilot/T34TDT_20190701T093041_B02_10m.jp2"
 
-gdalbuildvrt -separate rgb.vrt /vsicurl/${WPS_INPUT_RedBand} /vsicurl/${WPS_INPUT_GreenBand} /vsicurl/${WPS_INPUT_BlueBand}
+gdalbuildvrt -separate rgb.vrt "/vsicurl/${WPS_INPUT_RedBand}" "/vsicurl/${WPS_INPUT_GreenBand}" "/vsicurl/${WPS_INPUT_BlueBand}"
 
 echo "Saving output to: ${WPS_OUTPUT_Image}"
 
